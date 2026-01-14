5096a69001a59162fcd713b8b8b4ff8380cae07a
Tom Jenkin <TomJenkin@users.noreply.github.com>
2026-01-14 16:37:01 +0400

    update

--- data-table-3.rkt
+++ data-table-3.rkt
@@ -1,10 +1,11 @@
 #lang racket
 
-(require csv-reading
-         racket/date
-         racket/function
-         racket/list
-         plot)
+(require plot
+         csv-reading
+         ;;racket/date
+         ;;racket/function
+         ;;racket/list
+         )
 
 #| ======================== utilities ======================== |#
 
@@ -55,6 +56,21 @@
   (for/hash ([(k v) (in-hash dt)])
     (if (member k ns) (values k (map fn v)) (values k v))))
 
+;; convert from string to date
+(define (string->date-iso str)
+  (apply (λ (y m d) (date 0 0 0 d m y 0 0 #f 0))
+         (map string->number (string-split str "-"))))
+
+;; extend the data frame
+(define (dt-append dt key value)
+  (hash-set dt key value))
+
+;; assert data frame is valid
+(define (dt-assert-valid dt)
+  (define lengths-hash (make-hash (hash-map dt (lambda (key value) (cons key (length value))))))
+  (unless (apply = (hash-values lengths-hash))
+    (error 'dt-series-same-length "Series lengths are not the same")))
+
 ;; pipleline functions
 (define dt-generate (compose-pipe
                      read-csv
@@ -63,14 +79,39 @@
                      dt-create
                      (lambda (dt) (dt-rename dt (hash "SP500" "close" "observation_date" "date")))
                      (lambda (dt) (dt-retype dt string->number (list "close")))
-                     ))
+                     (lambda (dt) (dt-retype dt string->date-iso (list "date")))))
 
 #| ======================== executions ======================== |#
 
-(define dt (dt-generate "C:/Users/tomje/Downloads/SP500.csv"))
-
+(define tic "SP500")
+(define file-path "C:/Users/tomje/Downloads/")
+(define file-name (string-append file-path tic ".csv"))
+(define dt (dt-generate file-name))
 (dt-head dt 3)
+(dt-assert-valid dt)
 
-;;(dt-rename dt (hash "SP500" "tic"))
+(define dt2 (dt-append dt "test" (list 44 55 88 44)))
+(dt-head dt2 3)
 
+;;(dt-assert-valid dt2)
+;;(hash-values dt)
+;;(hash-keys dt)
+;;(dt-transpose (hash-values dt))
 
+#| ======================== plots ======================== |#
+
+(parameterize
+    ([plot-x-ticks (date-ticks)]
+     [plot-width 600]
+     [plot-height 300])
+  (plot
+   (lines
+    (map vector
+         ;;(map date->seconds (hash-ref dt "date"))
+         (map datetime->real (hash-ref dt "date"))
+         (hash-ref dt "close")))
+   #:x-label "Date"
+   #:y-label "Value"
+   ;;#:aspect-ratio #f
+   #:out-file (string-append file-path "test.png")
+   #:title (string-append "Market: " tic)))
