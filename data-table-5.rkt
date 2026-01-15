f1967743385f16d7dcf1bc3c4266b12c9247f445
Tom Jenkin <TomJenkin@users.noreply.github.com>
2026-01-15 09:49:47 +0400

    update

--- data-table-5.rkt
+++ data-table-5.rkt
@@ -2,7 +2,13 @@
 
 (require rackunit)
 
-(provide dataframe dataframe-ref dataframe-set dataframe-remove dataframe-update)
+(provide dataframe
+         dataframe-ref
+         dataframe-set
+         dataframe-remove
+         dataframe-update
+         dataframe-has-key
+         dataframe-shape)
 
 #| =================== structs =================== |#
 
@@ -22,7 +28,7 @@
     (unless (list? v)
       (error type-name "value for key ~e is not a list: ~e" k v)))
   
-  ;; validate all lists have same length
+  ;; validate all lists have same length or hash is empty
   (unless (or (hash-empty? hash)
               (apply = (map length (hash-values hash))))
     (error type-name
@@ -38,24 +44,31 @@
 #| =================== functions =================== |#
 
 ;; immutable ref
-(define (dataframe-ref dt key [default #f])
-  (hash-ref (dataframe-hash dt) key default))
+(define (dataframe-ref df key [default #f])
+  (hash-ref (dataframe-hash df) key default))
 
 ;; immutable set
-(define (dataframe-set dt key value)
-  (dataframe (hash-set (dataframe-hash dt) key value)))
+(define (dataframe-set df key value)
+  (dataframe (hash-set (dataframe-hash df) key value)))
 
 ;; immutable remove
-(define (dataframe-remove dt key)
-  (dataframe (hash-remove (dataframe-hash dt) key)))
+(define (dataframe-remove df key)
+  (dataframe (hash-remove (dataframe-hash df) key)))
 
 ;; immutable update
-(define (dataframe-update dt key updater)
-  (dataframe (hash-update (dataframe-hash dt) key updater)))
+(define (dataframe-update df key updater)
+  (dataframe (hash-update (dataframe-hash df) key updater)))
 
 ;; immutable has key
-(define (dataframe-has-key dt key)
-  (hash-has-key? (dataframe-hash dt) key))
+(define (dataframe-has-key df key)
+  (hash-has-key? (dataframe-hash df) key))
+
+;; immutable shape
+(define (dataframe-shape df)
+  (define col-length (hash-count (dataframe-hash df)))
+  (define first-key (car (hash-keys (dataframe-hash df))))
+  (define row-length (length (dataframe-ref df first-key)))
+  (list row-length col-length))
 
 #| =================== testing =================== |#
 
@@ -68,6 +81,7 @@
 
   (define df1 (dataframe (hash 'a '(1 2 3) 'b '(4 5 6))))
   (check-equal? (dataframe-ref df1 'a) '(1 2 3))
+  (check-equal? (dataframe-ref df1 'missing-key 'na) 'na)
 
   (define df2 (dataframe-set df1 'c '(5 6 7)))
   (check-equal? (dataframe-ref df2 'c) '(5 6 7))
@@ -76,10 +90,13 @@
   (check-equal? df1 df3)
 
   (define (double-list lst) (map (lambda (x) (* 2 x)) lst))
-  (define dt4 (dataframe-update df1 'a double-list))
-  (check-equal? (dataframe-ref dt4 'a) '(2 4 6))
+  (define df4 (dataframe-update df1 'a double-list))
+  (check-equal? (dataframe-ref df4 'a) '(2 4 6))
 
   (check-equal? (dataframe-has-key df1 'a) #t)
+
+  (define df5 (dataframe-shape df1))
+  (check-equal? df5 '(3 2))
   
   (displayln "testing: success!")
   
