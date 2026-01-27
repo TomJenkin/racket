afee1ccb374ed270de5e96a4efa5e85e6ff7f816
Tom Jenkin <TomJenkin@users.noreply.github.com>
2026-01-27 22:37:11 +0400

    update

--- haar.rkt
+++ haar.rkt
@@ -1,15 +1,18 @@
 #lang racket
 
-(require rackunit
+(require plot
+         ;;racket/gui/base
+         rackunit
          syntax/location
          (prefix-in gt: "gen-tools.rkt"))
 
 (provide haar
          haar-inv
-         haar-slice)
+         haar-filter)
 
 (define module-name (path->string (syntax-source-file-name #'here)))
 
+;; haar transform
 (define (haar xs)
   (define n (length xs))
   (unless (even? n) (error 'haar1 "list length must be even"))
@@ -20,7 +23,7 @@
                (* k (- (list-ref xs i) (list-ref xs (add1 i))))))
   (append As Ds))
 
-;; inverse haar
+;; haar inverse transform
 (define (haar-inv coeffs)
   (define n (length coeffs))
   (unless (even? n) (error 'inv-haar1 "coeff length must be even"))
@@ -33,7 +36,8 @@
      (list (* k (+ a d))
            (* k (- a d))))))
 
-(define (haar-slice coeffs a b)
+;; haar filter (e.g. high-pass, low-pass)
+(define (haar-filter coeffs a b)
   (unless (and (real? a) (real? b) (<= 0 a b 1))
     (error 'haar-slice "need 0 <= a <= b <= 1"))
   (define n (length coeffs))
@@ -48,23 +52,64 @@
   (for/list ([c (in-list coeffs)] [i (in-naturals)])
     (if (set-member? keep i) c 0.0)))
 
-(time
- (test-case
-  "haar tests"
-  ; recovery tests
-  (define (round-10 ls) (gt:round-n ls 10)) 
-  (define fn1 (compose round-10 haar-inv haar))
-  ;;(define xs '(100 101 99 102 100 101 99 103))
-  (define xs (for/list ([i 1000]) (+ 100 (random 21))))
-  (define xss (map exact->inexact xs))
-  (check-equal? (fn1 xs) xss)
-  (define ys (haar xs))
-  (check-equal? (round-10 (haar-inv (haar-slice ys 0 1))) (map exact->inexact xs))
-  ; check that hp + lp == original series
-  (for/list ([e '(0.25 0.5 0.75)])
-    (define zs (haar-inv (haar-slice ys 0 e)))
-    (define ws (haar-inv (haar-slice ys e 1)))
-    (define es (round-10 (map + ws zs)))
-    (define xss (map exact->inexact xs))
-    (check-equal? es xss))
-  (displayln (string-append module-name ": passed tests"))))
+
+
+(module+ test
+
+  (test-case
+   "haar tests"
+   ; recovery tests
+   (define (round-10 ls) (gt:round-n ls 10)) 
+   (define fn1 (compose round-10 haar-inv haar))
+   ;;(define xs '(100 101 99 102 100 101 99 103))
+   (define xs (for/list ([i 1000]) (+ 100 (random 21))))
+   (define xss (map exact->inexact xs))
+   (check-equal? (fn1 xs) xss)
+   (define ys (haar xs))
+   (check-equal? (round-10 (haar-inv (haar-filter ys 0 1))) (map exact->inexact xs))
+   ; check that hp + lp == original series
+   (for/list ([e '(0.05 0.1 0.25 0.5 0.75 0.9 0.95)])
+     (define zs (haar-inv (haar-filter ys 0 e)))
+     (define ws (haar-inv (haar-filter ys e 1)))
+     (define es (round-10 (map + ws zs)))
+     (define xss (map exact->inexact xs))
+     (check-equal? es xss)))
+ 
+  ;; plots
+
+  (define (random-walk n [start 100])
+    (reverse
+     (for/fold ([path (list start)]) ([i (in-range (sub1 n))])
+       (define step (if (zero? (random 2)) -1 1))
+       (cons (+ (car path) step) path))))
+  
+  ;;(define xss (for/list ([i 1000]) (+ 100 (random 21))))
+  (define xs (random-walk 1000 100))
+  ;;(define xs (map exact->inexact xss))
+  (define hs (haar xs))
+
+  (define cut 0.8)
+  
+  (define fs1 (haar-inv (haar-filter hs 0 cut)))
+
+  (define fs2 (haar-inv (haar-filter hs cut 1)))
+
+  ;;(lines (map vector (map date->seconds dates) values)) ; timeseries
+
+  (parameterize ([plot-x-label "Index"]
+                 [plot-y-label "Value"]
+                 [plot-width 500]
+                 [plot-height 250])
+    ;;(plot (lines (map vector (range (length hs)) hs)))
+
+    (plot (list 
+           (lines (map vector (range (length xs)) xs))
+           (lines (map vector (range (length xs)) fs1))
+           (lines (map vector (range (length xs)) fs2))))
+
+
+    )
+  
+  (displayln (string-append module-name ": passed all tests!"))
+
+  )
