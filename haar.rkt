6ec2a932e0ba41338cb3242e514280988292eed6
Tom Jenkin <TomJenkin@users.noreply.github.com>
2026-01-28 12:16:03 +0400

    update

--- haar.rkt
+++ haar.rkt
@@ -7,11 +7,12 @@
 
 (provide haar
          haar-inv
-         haar-filter)
+         ;haar-filter
+         )
 
 (define module-name (path->string (syntax-source-file-name #'here)))
 
-;; haar transform
+;; haar transform - 1 level
 (define (haar xs)
   (define n (length xs))
   (unless (even? n) (error 'haar1 "list length must be even"))
@@ -22,7 +23,7 @@
                (* k (- (list-ref xs i) (list-ref xs (add1 i))))))
   (append As Ds))
 
-;; haar inverse transform
+;; haar inverse transform - 1 level
 (define (haar-inv coeffs)
   (define n (length coeffs))
   (unless (even? n) (error 'inv-haar1 "coeff length must be even"))
@@ -35,7 +36,8 @@
      (list (* k (+ a d))
            (* k (- a d))))))
 
-;; haar filter (e.g. high-pass, low-pass)
+#|
+;; haar filter (e.g. high-pass, low-pass) - 1 level
 (define (haar-filter coeffs a b)
   (unless (and (real? a) (real? b) (<= 0 a b 1))
     (error 'haar-slice "need 0 <= a <= b <= 1"))
@@ -50,8 +52,21 @@
   (define keep (list->set (take (drop order lo) (- hi lo))))
   (for/list ([c (in-list coeffs)] [i (in-naturals)])
     (if (set-member? keep i) c 0.0)))
+|#
 
+;; 1 level
+(define (haar-lowpass coeffs)
+  (define n (length coeffs))
+  (define half (quotient n 2))
+  (append (take coeffs half)
+          (make-list half 0.0)))
 
+;; 1 level
+(define (haar-highpass coeffs)
+  (define n (length coeffs))
+  (define half (quotient n 2))
+  (append (make-list half 0.0)
+          (drop coeffs half)))
 
 (module+ test
 
@@ -65,17 +80,16 @@
    (define xss (map exact->inexact xs))
    (check-equal? (fn1 xs) xss)
    (define ys (haar xs))
-   (check-equal? (round-10 (haar-inv (haar-filter ys 0 1))) (map exact->inexact xs))
-   ; check that hp + lp == original series
-   (for/list ([e '(0.05 0.1 0.25 0.5 0.75 0.9 0.95)])
-     (define zs (haar-inv (haar-filter ys 0 e)))
-     (define ws (haar-inv (haar-filter ys e 1)))
+   ;;(check-equal? (round-10 (haar-inv (haar-filter ys 0 1))) (map exact->inexact xs))
+   (for ([e '(0.05 0.1 0.25 0.5 0.75 0.9 0.95)])
+     (define zs (haar-inv (haar-lowpass ys)))
+     (define ws (haar-inv (haar-highpass ys)))
      (define es (round-10 (map + ws zs)))
-     (define xss (map exact->inexact xs))
      (check-equal? es xss)))
- 
+
   ;; plots
 
+  (random-seed 0)
   (define (random-walk n [start 100])
     (reverse
      (for/fold ([path (list start)]) ([i (in-range (sub1 n))])
@@ -87,14 +101,22 @@
   ;;(define xs (map exact->inexact xss))
   (define hs (haar xs))
 
-  (define cut 0.98)
+  (define cut 0.5)
   
-  (define fs1 (haar-inv (haar-filter hs 0 cut)))
+  ;;(define fs1 (haar-inv (haar-filter hs 0 cut)))
+  ;;(define fs2 (haar-inv (haar-filter hs cut 1)))
 
-  (define fs2 (haar-inv (haar-filter hs cut 1)))
+  ;;(define fs1 (haar-inv (haar-filter-mass hs 0 cut)))
+  ;;(define fs2 (haar-inv (haar-filter-mass hs cut 1)))
+
+  (define fs1 (haar-inv (haar-lowpass hs)))
+  (define fs2 (haar-inv (haar-highpass hs)))
 
   ;;(lines (map vector (map date->seconds dates) values)) ; timeseries
 
+  
+  (plot-new-window? #f)
+  
   (parameterize ([plot-x-label "Index"]
                  [plot-y-label "Value"]
                  [plot-width 500]
@@ -102,15 +124,14 @@
     ;;(plot (lines (map vector (range (length hs)) hs)))
 
     (plot (list 
-           ;;(lines (map vector (range (length xs)) xs))
+           (lines (map vector (range (length xs)) xs))
            (lines (map vector (range (length xs)) fs1))
-           ;;(lines (map vector (range (length xs)) fs2))
-           ))
-
-
-    
-
+           (lines (map vector (range (length xs)) fs2))
+           )
+          #:out-file "C:/Users/tomje/Downloads/test-plot.png")
     )
+
+
   
   (displayln (string-append module-name ": passed all tests!"))
 
