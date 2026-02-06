51d9c4c544a3a451f12cb98689ebc5effb1d5b39
Tom Jenkin <TomJenkin@users.noreply.github.com>
2026-02-06 17:35:07 +0000

    update

--- test-2.rkt
+++ test-2.rkt
@@ -29,3 +29,34 @@ data
 (define b 3)
 
 (string-replace (jsexpr->string (hash 'a a 'b b)) "\"" "\"\"")
+
+; cool stuff
+
+
+;; The famous Y combinator - enables recursion without self-reference
+(define Y
+  (λ (f)
+    ((λ (x) (f (λ (y) ((x x) y))))
+     (λ (x) (f (λ (y) ((x x) y)))))))
+
+;; Factorial using Y combinator - no named recursion!
+(define factorial
+  (Y (λ (fact)
+       (λ (n)
+         (if (zero? n)
+             1
+             (* n (fact (- n 1))))))))
+
+;; Test it
+(factorial 5)  ; => 120
+
+;; Fibonacci using Y combinator
+(define fibonacci
+  (Y (λ (fib)
+       (λ (n)
+         (cond [(= n 0) 0]
+               [(= n 1) 1]
+               [else (+ (fib (- n 1)) 
+                        (fib (- n 2)))])))))
+
+(fibonacci 10)  ; => 55
