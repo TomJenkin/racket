b411a51fe56318fb8312d5d17acea8caf5c434ac
Tom Jenkin <TomJenkin@users.noreply.github.com>
2026-02-21 05:05:37 +0000

    update

--- tester.rkt
+++ tester.rkt
@@ -1,9 +1,8 @@
 #lang racket
 
-(require 
-  ;;rackunit
-  ;math/statistics
-  ;racket/date
+(require
+  plot
+  racket/date
   (prefix-in dt: "data-table.rkt")
   (prefix-in gt: "gen-tools.rkt")
   (prefix-in cl: "cluster-kmeans.rkt")
@@ -18,34 +17,6 @@
   (if (zero? range)
       (map (λ (_) 0.0) xs)   ; all values identical → all zeros
       (map (λ (x) (/ (- x mn) range)) xs)))
-
-
-
-#| 
-   (define data
-     (let* ([data-1 sd:data-sp500]
-            [rolling-win-length 32]
-            [tail (gt:rolling (λ (e) e) rolling-win-length (dt:table-read data-1 "close"))]
-            [data-2 (dt:table-dropna (dt:table-create data-1 "tail" tail))]
-            [fn (compose wt:haar normalize)]
-            [data-3 (dt:table-create data-2 "tail-rel" (map fn (dt:table-read data-2 "tail")))]
-            [n-clusters 6]
-            ;[cluster-hash (call-with-values
-            ;               (λ () (cl:kmeans (dt:table-read data-3 "tail-rel") n-clusters 5000)) 
-            ;               (λ (x y) (hash 'means x 'assigns y)))]
-   
-            [cluster-hash (cl:kmeans (dt:table-read data-3 "tail-rel") n-clusters 5000)]
-            
-            [data-4 (dt:table-create data-3 "label" (hash-ref cluster-hash 'assignments))]
-            [data (dt:table-tail data-4 5)]
-            )
-       (dt:table-print data-1 5 #:head #f)
-       data))
-   
-   (define dates (dt:table-read data "date"))
-   (define values (dt:table-read data "label"))
-   
- |#
 
 (define data
   (let* ([data-1 sd:data-sp500]
@@ -65,3 +36,49 @@
 (define dates (dt:table-read data "date"))
 (define values (dt:table-read data "label"))
 
+
+
+
+;; PLOTS
+
+;; plot cluster groups/labels
+(define (plot-dates-values dates values)
+  (parameterize ([plot-x-ticks (date-ticks)]
+                 [plot-width 500]
+                 [plot-height 250])
+    (plot
+     (lines
+      (map vector
+           (map date->seconds dates)
+           values))
+     #:x-label "Date"
+     #:y-label "Value")))
+
+(plot-dates-values dates values)
+
+
+#| 
+   
+   ;; plot cluster means
+   (define (plot-many-series series-list)
+     (define renderers
+       (for/list ([ys series-list] [i (in-naturals 1)])
+         (define pts
+           (for/list ([y ys] [x (in-naturals 0)])
+             (vector x y)))
+         (lines pts
+                ;;#:label (format "series ~a" i)
+                )))
+   
+     (parameterize ([plot-x-label "Index"]
+                    [plot-y-label "Value"]
+                    [plot-width 500]
+                    [plot-height 250]
+                    ;;[plot-x-grid? #t]
+                    ;;[plot-y-grid? #t]
+                    )
+       (plot renderers)))
+   
+   (plot-many-series means-2)
+   
+ |#
