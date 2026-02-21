4632477c3caa12df56e58777230e365cb6fa574c
Tom Jenkin <TomJenkin@users.noreply.github.com>
2026-02-21 13:21:47 +0000

    update

--- tester.rkt
+++ tester.rkt
@@ -18,67 +18,65 @@
       (map (λ (_) 0.0) xs)   ; all values identical → all zeros
       (map (λ (x) (/ (- x mn) range)) xs)))
 
-(define data
-  (let* ([data-1 sd:data-sp500]
-         [rolling-win-length 32]
-         [tail (gt:rolling (λ (e) e) rolling-win-length (dt:table-read data-1 "close"))]
-         [data-2 (dt:table-dropna (dt:table-create data-1 "tail" tail))]
-         [fn (compose wt:haar normalize)]
-         [data-3 (dt:table-create data-2 "tail-rel" (map fn (dt:table-read data-2 "tail")))]
-         [n-clusters 6]
-         [cluster-hash (cl:kmeans (dt:table-read data-3 "tail-rel") n-clusters 5000)]
-         [data-4 (dt:table-create data-3 "label" (hash-ref cluster-hash 'assignments))]
-         [data (dt:table-tail data-4 5)]
-         )
-    (dt:table-print data-1 5 #:head #f)
-    data))
+(define (process-data)
+  (define data-1 sd:data-sp500)
+  (define rolling-win-length 32)
+  (define tail (gt:rolling (λ (e) e) rolling-win-length (dt:table-read data-1 "close")))
+  (define data-2 (dt:table-dropna (dt:table-create data-1 "tail" tail)))
+  (define fn (compose wt:haar normalize))
+  (define data-3 (dt:table-create data-2 "tail-rel" (map fn (dt:table-read data-2 "tail"))))
+  (define n-clusters 6)
+  (define cluster-hash (cl:kmeans (dt:table-read data-3 "tail-rel") n-clusters 5000))
+  (define assignments (hash-ref cluster-hash 'assignments))
+  (define centroids   (hash-ref cluster-hash 'centroids))
+  (define data-4 (dt:table-create data-3 "label" assignments))
+  ;(define data (dt:table-tail data-4 5))
+  (define data data-4)
+  (hash 'data data 'data-1 data-1 'centroids centroids))
 
-(define dates (dt:table-read data "date"))
-(define values (dt:table-read data "label"))
+(define bundle (process-data))
+(define dates1 (dt:table-read (hash-ref bundle 'data) "date"))
+(define values1 (dt:table-read (hash-ref bundle 'data) "label"))
+(define means1 (hash-ref bundle 'centroids))
 
-
-
+(dt:table-print (hash-ref bundle 'data-1) 5 #:head #f)
 
 ;; PLOTS
 
 ;; plot cluster groups/labels
-(define (plot-dates-values dates values)
+(define (plot-dates-values dates1 values1)
   (parameterize ([plot-x-ticks (date-ticks)]
                  [plot-width 500]
                  [plot-height 250])
     (plot
      (lines
       (map vector
-           (map date->seconds dates)
-           values))
+           (map date->seconds dates1)
+           values1))
      #:x-label "Date"
      #:y-label "Value")))
 
-(plot-dates-values dates values)
+(plot-dates-values dates1 values1)
 
-
-#| 
+;; plot cluster means
+(define (plot-many-series series-list)
+  (define renderers
+    (for/list ([ys series-list] [i (in-naturals 1)])
+      (define pts
+        (for/list ([y ys] [x (in-naturals 0)])
+          (vector x y)))
+      (lines pts
+             ;;#:label (format "series ~a" i)
+             )))
    
-   ;; plot cluster means
-   (define (plot-many-series series-list)
-     (define renderers
-       (for/list ([ys series-list] [i (in-naturals 1)])
-         (define pts
-           (for/list ([y ys] [x (in-naturals 0)])
-             (vector x y)))
-         (lines pts
-                ;;#:label (format "series ~a" i)
-                )))
+  (parameterize ([plot-x-label "Index"]
+                 [plot-y-label "Value"]
+                 [plot-width 500]
+                 [plot-height 250]
+                 ;;[plot-x-grid? #t]
+                 ;;[plot-y-grid? #t]
+                 )
+    (plot renderers)))
    
-     (parameterize ([plot-x-label "Index"]
-                    [plot-y-label "Value"]
-                    [plot-width 500]
-                    [plot-height 250]
-                    ;;[plot-x-grid? #t]
-                    ;;[plot-y-grid? #t]
-                    )
-       (plot renderers)))
+(plot-many-series means1)
    
-   (plot-many-series means-2)
-   
- |#
